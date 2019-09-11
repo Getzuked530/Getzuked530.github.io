da158e7da5000453cc007dfc13866585b53e2381
Getzuked530 <steven.scratton@gmail.com>
2019-09-11 14:11:38 -0600

    Added interactive folder

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -6,12 +6,22 @@
 // - describe what you did to take this project "above and beyond"
 
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255,255,255);
+  background(255);
 }
 
 function draw() {
-  line(mouseX, mouseY, pmouseX, pmouseY);  
+  line(mouseX,mouseY,100,100)
 }
-key
+
+
+
+
+
+
+    
+  
+
+
