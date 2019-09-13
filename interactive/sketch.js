da7cd520e89717d6783d2d7e22fa99bed7b7799b
Getzuked530 <steven.scratton@gmail.com>
2019-09-13 14:11:31 -0600

    Added kitty and red

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -1,20 +1,36 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+let kitty;
+let sizeW = 175
+let sizeH = 200
+let bkColour = 255
+let message = "hello world"
 
 
+function preload() {
+  kitty = loadImage("assets/kitty.png")
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  
 }
 
 function draw() {
-  line(mouseX,mouseY,100,100)
+  background(bkColour);
+  imageMode(CENTER);
+  image(kitty, width / 2, height / 2, sizeW, sizeH);
+  text(message, width / 2, 100,)
+ 
+  }
+
+function mouseClicked(){
+  bkColour = "red"
+  //text(message, width / 2, 100,);
+  
 }
+
+
+  
+
 
 
 
