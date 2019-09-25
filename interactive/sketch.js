7fae969656dcdbd5868b33c345fbb40fc05722ab
Getzuked530 <steven.scratton@gmail.com>
2019-09-25 14:12:00 -0600

    added type function

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -8,7 +8,12 @@ let hurtMessage = ["OOOOW", "HOW COULD YOU", "YOU BASTARD YOU HURT HIM", "YOU LI
 let hmLast;
 let hmNew;
 let soundList = ["a","b","c","d","e"];
-
+let see = false;
+let aeh = false;
+let tea = false;
+let x;
+let y;
+let sadMessage = ["Oh you... you killed him. :("]
 function preload() {
   kitty = loadImage("assets/kitty.png");
   kittyHurt = loadImage("assets/DeadCat.jpg");
@@ -18,24 +23,34 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = width/2
+  y = height/2
+
+
+
 }
 
 function draw() {
   background(bkColour);
   imageMode(CENTER);
-  image(kitty, width / 2, height / 2, sizeW, sizeH);
+  image(kitty, x, y, sizeW, sizeH);
 
   textSize(32);
   textAlign(CENTER, TOP);
   text(bgMessage, width / 2,  100,);
  
-  }
+  textSize(10);
+  textAlign(BOTTOM, LEFT)
+  text("try typing 'cat'", 100, 50 )
+
+  heDead()
+
+}
 
 function mouseClicked(){
   
   bkColour = "red"
 
-  
   kitty = kittyHurt;
 
   
@@ -57,5 +72,46 @@ function mouseClicked(){
  // hmLast = bgMessage;
  // console.log(hmLast);
 
+}
+
+
+
+function windowResized(){
+  setup();
+}
+ 
+
+function keyTyped(){
+  if (key === "c"){
+    see = true;
+    console.log(see);
+  }
+  else if (key === "a"){
+    aeh = true;
+    console.log(aeh);
+  }
+
+  else if (key === "t"){
+    tea = true;
+    console.log(tea);
+  }
+
+}
+
+function heDead(){
+  if (see === true && aeh === true && tea === true){
+    y -= 2
+    bgMessage = sadMessage
+    if ( y <= -100){
+      see = false;
+      aeh = false;
+      tea = false;
+      location.reload();
+    }
+
+
+
+    console.log("Paradise");
+  }
 }
 
