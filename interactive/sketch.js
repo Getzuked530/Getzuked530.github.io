c33cf95bd6b928ed4d247078b41b2d952cbdd716
Getzuked530 <45461881+Getzuked530@users.noreply.github.com>
2019-09-25 23:20:29 -0600

    fINISHED PROJECT

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -1,3 +1,5 @@
+
+
 let kitty;
 let sizeW = 175;
 let sizeH = 200;
@@ -7,43 +9,55 @@ let kittyHurt;
 let hurtMessage = ["OOOOW", "HOW COULD YOU", "YOU BASTARD YOU HURT HIM", "YOU LIKE HITTING KITTIES HUH","YOU MONSTER","WHY ARE YOU SO MEAN HURTING THIS POOR BOY" ];
 let hmLast;
 let hmNew;
-let soundList = ["a","b","c","d","e"];
 let see = false;
 let aeh = false;
 let tea = false;
 let x;
 let y;
 let sadMessage = ["Oh you... you killed him. :("]
+let meme;
+let wolvesPlay = true;
+
+
 function preload() {
+  //Loading images for normal and hurt cat
   kitty = loadImage("assets/kitty.png");
   kittyHurt = loadImage("assets/DeadCat.jpg");
+
+  soundFormats("wav")
+  //Loads sound for secret event
+  meme = loadSound("assets/WooAhh.wav")
 
   
 }
 
+//Creating canvas and placment of image
 function setup() {
   createCanvas(windowWidth, windowHeight);
   x = width/2
   y = height/2
 
-
-
 }
 
 function draw() {
+
+  //Dictating background color and current image
   background(bkColour);
   imageMode(CENTER);
   image(kitty, x, y, sizeW, sizeH);
 
-  textSize(32);
+  //Text for insult message
+  textSize(40);
   textAlign(CENTER, TOP);
   text(bgMessage, width / 2,  100,);
  
-  textSize(10);
+  //Hint message at top left
+  textSize(14);
   textAlign(BOTTOM, LEFT)
   text("try typing 'cat'", 100, 50 )
 
-  heDead()
+  //Activates the stupid hidden meme when reqiuerments met
+  heDeadMovement()
 
 }
 
@@ -57,7 +71,7 @@ function mouseClicked(){
   bgMessage = random(hurtMessage);
   hmNew = bgMessage; 
 
-
+  // Checks for duplicates and replaces them with new message until different
   while (hmLast === hmNew ){
     hmNew = random(hurtMessage);
     bgMessage = hmNew;
@@ -66,6 +80,7 @@ function mouseClicked(){
     console.log("dupe");
   }
 
+  //essential debugging for dupe checking
   hmLast = hmNew;
   console.log(hmLast);
 
@@ -82,27 +97,47 @@ function windowResized(){
  
 
 function keyTyped(){
-  if (key === "c"){
+
+  //saving variables
+  if (key === "c" || key === "C"){
     see = true;
     console.log(see);
   }
-  else if (key === "a"){
+  else if (key === "a" || key === "A"){
     aeh = true;
     console.log(aeh);
   }
 
-  else if (key === "t"){
+  else if (key === "t" || key === "T"){
     tea = true;
     console.log(tea);
   }
 
+  //Sound Test
+ // else if (key === "w"){
+  //  meme.play()
+ // }
+
 }
 
-function heDead(){
+function heDeadMovement(){
+  //Checks if each variable is true 
   if (see === true && aeh === true && tea === true){
-    y -= 2
+
+    // Plays sound only once while in draw loop by turning itself false after it runs once
+    if (wolvesPlay === true){
+      meme.play()
+      wolvesPlay = false;
+    }
+
+    //Moves current cat upwards
+    y -= 1
+
+
     bgMessage = sadMessage
-    if ( y <= -100){
+
+    //Once image leaves frame for long enough scene resets
+    if ( y <= -150){
       see = false;
       aeh = false;
       tea = false;
@@ -111,7 +146,7 @@ function heDead(){
 
 
 
-    console.log("Paradise");
+    
   }
 }
 
