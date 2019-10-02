c9b874e7bdd371f185fea7571d46f4a9ba4f77fa
Getzuked530 <steven.scratton@gmail.com>
2019-10-02 14:11:35 -0600

    delted useless stuff and added to state variable\

--- surreal/sketch.js
+++ surreal/sketch.js
@@ -1,5 +1,5 @@
 
-
+menuState = "startMenu"
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
@@ -8,4 +8,28 @@ function setup() {
 
 function draw() {
   background(255)
+
+  if (menuState === "startMenu"){
+    startMenu();
+  }
+
+  else if (menuState === "game"){
+    showGame();
+
+  }
+
+  else if (menuState === "options"){
+    showOptions();
+
+  }
+
+}
+
+function startMenu(){
+  rectMode(CENTER);
+  fill(0, 0, 255, 200);
+  rect(width/2 - 200, height/2 , 300, 150)
+
+  fill(0, 0, 255, 200);
+  rect(width/2 + 200, height/2 , 300, 150)
 }
