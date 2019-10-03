4797dba43e8fc4d5bb1ea0714507442bf13db497
Getzuked530 <steven.scratton@gmail.com>
2019-10-03 14:15:59 -0600

    idea put in

--- dating-sim/sketch.js
+++ dating-sim/sketch.js
@@ -1,9 +1,17 @@
 
-menuState = "startMenu"
+menuState = "startMenu";
+noEscape = "Options? What options? You are playing a Shrek dating sim. What did you think you would find in here.";
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
 
+}
+
+function windowResized(){
+  setup();
+
+  startMenu();
 }
 
 function draw() {
@@ -32,4 +40,12 @@ function startMenu(){
 
   fill(0, 0, 255, 200);
   rect(width/2 + 200, height/2 , 300, 150)
+}
+
+function showOptions(){
+  textAlign(CENTER, TOP,);
+  textSize(30)
+  text("hello world")
+
+  
 }
