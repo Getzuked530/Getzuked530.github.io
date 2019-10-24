5904d3028415f2d5fe7cf9b4e21d1efc7632646f
Getzuked530 <steven.scratton@gmail.com>
2019-10-24 14:16:29 -0600

    gridstuff

--- 2dgrid/sketch.js
+++ 2dgrid/sketch.js
@@ -7,17 +7,27 @@ function setup() {
 
 function draw() {
   background(220)
+  displayGrid(grid);
+
+}
+
+
+function mousePressed() {
+  
+  console.log
+  console.log(mouseX, mouseY)
 }
 
 function displayGrid(theGrid) {
   for (let y = 0; y < theGrid[0].length; y++){
     for (let x = 0; x <theGrid[0].length; x++){
       if (theGrid[y][x] === 0) {
-        fill(255)
+        fill(255);
 
       }
       else {
         fill(0);
+      
       }
       let cellSize = width/theGrid[0].length;
       rect(x * cellSize, y * cellSize, cellSize,cellSize)
@@ -30,15 +40,15 @@ function create2dArray(cols, rows) {
   let someArray = [];
   for (let i = 0; i<cols; i++) {
     someArray.push([]);
-     for (let j = 0; j<rows; j++) {
-        if (random((100) < 50) {
-        someArray[i].push(1);
-        }
-        else{
-          someArray[i].push(0)
-        }
+    for (let j = 0; j<rows; j++) {
+      if (random(100) < 50) {
+      someArray[i].push(1);
+      }
+      else {
+        someArray[i].push(0)
+      }
     }
-    return someArray;
-  }
   
-
+  }
+  return someArray;
+}
