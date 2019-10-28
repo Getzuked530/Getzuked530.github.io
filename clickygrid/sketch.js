fb94426b169bd0556bc68bca82b29ea254314f5c
Getzuked530 <steven.scratton@gmail.com>
2019-10-28 14:11:22 -0600

    conway life

--- clickygrid/sketch.js
+++ clickygrid/sketch.js
@@ -1,14 +1,13 @@
-  
 let grid;
 let rows = 30;
 let cols = 30;
 
 function setup() {
-  if (windowWidth > windowHeight){
+  if (windowWidth > windowHeight) {
     createCanvas(windowHeight, windowHeight);
   }
   else {
-    createCanvas(windowWidth, windowWidth)
+    createCanvas(windowWidth, windowWidth);
   }
   grid = createRandom2dArray(cols, rows);
 }
@@ -16,23 +15,29 @@ function setup() {
 function draw() {
   background(220);
   displayGrid(grid, rows, cols);
+  update();
+}
+
+function windowResized() {
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
 }
 
 function keyTyped() {
-  if (key === "r"){
-    grid = createRandom2dArray(cols, rows)
-
+  if (key === "r") {
+    grid = createRandom2dArray(cols, rows);
   }
-  if (key === "c"){
-    for (let x = 0; x <cols; x++){
-      for ( let y = 0; y < rows; y++){
-        grid[y][x] = 1;
-      }
-    }
+  if (key === "c") {
+    grid = createEmptyGrid();
   }
-
+  if (key === " ") {
+    update();
+  }
 }
-
 
 function mousePressed() {
   let cellSize = width/cols;
@@ -48,15 +53,56 @@ function mousePressed() {
   }
 }
 
+function update() {
+  let nextTurn = createEmptyGrid();
+
+  for (let x = 0; x < cols; x++) {
+    for(let y = 0; y < rows; y++) {
+      let neighbors = 0;
+
+      for (let i = -1; i <= 1; i++){
+        for (let j = -1; j <= 1; j++){
+          //deal with edge cases
+          if (x+1 >= 0 && x+1 < cols && y+j >= 0 && y+j < rows){
+            neighbors += grid[y+j][x+i];
+          }
+        }
+      }
+      //dont count self as neighbor
+      neighbors -= grid[y][x]
+
+      //apply rules
+      if (grid[y][x] === 1) { // currently alive
+        if (neighbors === 2 || neighbors === 3){
+          nextTurn[y][x] = 1;
+        }
+        else{
+          nextTurn[y][x] = 0;
+        }
+      }
+
+      if (grid[y][x] === 0){
+        if(neighbors === 3){
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+    }
+  }
+  grid = nextTurn
+}
+
 function displayGrid(grid, rows, cols) {
   let cellSize = width / cols;
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       if (grid[y][x] === 0) {
-        fill(0);
+        fill(255);
       }
       else {
-        fill(255);
+        fill(0);
       }
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
@@ -79,3 +125,13 @@ function createRandom2dArray(cols, rows) {
   return randomGrid;
 }
 
+function createEmptyGrid() {
+  let emptyGrid = [];
+  for (let x = 0; x < cols; x++) {
+    emptyGrid.push([]);
+    for (let y = 0; y < rows; y++){
+      emptyGrid[x].push(0);
+    }
+  }
+  return emptyGrid;
+}
